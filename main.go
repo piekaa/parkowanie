4e85d14b2e38105800e9c6286ee10c2c8b370031
piotriwaniuk <piekoszek@gmail.com>
2021-11-11 19:08:34 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Static("/", "static")
+
+	e.Logger.Fatal(e.Start(":6812"))
+}
